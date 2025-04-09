d10e12be734b6a9f21d83521d6d355f2605248fa
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2025-04-09 18:44:43 +0300

    version: 0.9.28

--- src/components/Recipes/data/data_soup.jsx
+++ src/components/Recipes/data/data_soup.jsx
@@ -1,6 +1,11 @@
 import soup_01 from '../img/soup_01.jpg';
+import soup_02_00 from '../img/soup/02-00.png';
+import soup_02_01 from '../img/soup/02-01.png';
+import soup_02_02 from '../img/soup/02-02.png';
+import soup_02_03 from '../img/soup/02-03.png';
+import soup_02_04 from '../img/soup/02-04.png';
 
-// import cake_00 from '../img/load.gif';
+import cake_00 from '../img/load.gif';
 
 export const data_soup = [
   {
@@ -36,6 +41,47 @@ export const data_soup = [
       },
     ],
   },
+  {
+    id: '2',
+    name: 'Турецкий грибной суп',
+    img: soup_02_00,
+    ingredients: [
+      { i_name: 'Лук', i_weight: '2 шт.' },
+      { i_name: 'Чеснок', i_weight: '5 зубчиков' },
+      { i_name: 'Сливочное масло', i_weight: '1 ст.л.' },
+      { i_name: 'Растительное масло', i_weight: '2 ст.л.' },
+      { i_name: 'Грибы', i_weight: '800 гр.' },
+      { i_name: 'Мука', i_weight: '3 ст.л.' },
+      { i_name: 'Вода', i_weight: '2 литра' },
+      { i_name: 'Сливки', i_weight: '200 мл' },
+      { i_name: 'Черный перец', i_weight: '1 ч.л.' },
+      { i_name: 'Соль', i_weight: '1 ч.л.' },
+      { i_name: 'Укроп', i_weight: '1/2 пучка' },
+    ],
+    steps: [
+      {
+        step: 1,
+        img: soup_02_01,
+        text: 'На сливочно оливковом масле обжареваем лук и чеснок.',
+      },
+      {
+        step: 2,
+        img: soup_02_02,
+        text: 'К обжареному луку добавляем грибы и обжареваем, по готовности грибов добавляем 3 ст.л. муки, перемешиваем  и готовим около минуты.',
+      },
+      {
+        step: 3,
+        img: soup_02_03,
+        text: 'Солим грибы и к ним добавляем кипяток и варим 15 минут',
+      },
+      {
+        step: 4,
+        img: soup_02_04,
+        text: 'Добавить сливки, перец, укроп и довести до кипения. Суп готов!',
+      },
+    ],
+  },
+  // ***
   // {
   //   id: '21',
   //   name: 'Шаблон 1',
